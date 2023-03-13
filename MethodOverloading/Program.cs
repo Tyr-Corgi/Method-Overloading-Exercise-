60dd20300acacd1fde20e25fcea8da34e32292cf
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-13 15:10:35 -0600

    Completed

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,44 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(9, -1);
+            Add(1.1, 1.2);
+            Add(1, 0, true);
+        }
+        public static int Add(int x, int y)
+        {
+            Console.WriteLine($"{x} added to {y} is equal to {x + y}");
+         return x + y;
+        }
+        public static double Add(double x, double y)
+        {
+            Console.WriteLine($"{x} add to {y} is equal to {x + y}");
+         return x + y;
+        }
+        public static string Add(int x, int y, bool isCheck)
+        {
+            var total = x + y;
+            if (isCheck == true && total > 1)
+            {
+                 Console.WriteLine($"You have {total} dollars");
+                return $"{total} dollars";
+            }
+            else if (isCheck == true && total == 1)
+            {
+                Console.WriteLine($"You have {total} dollar");
+                return $"You have {total} dollar";
+            }
+            else if (isCheck == true && total < 1)
+            {
+                Console.WriteLine($"You have {total} cents");
+                return ($"You have {total} cents");          
+            }
+            else
+            {
+                return total.ToString();
+            }
         }
     }
 }
